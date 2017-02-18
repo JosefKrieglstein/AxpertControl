9ab5b3adb2e71fa75da6a31f33a35671eec9ad27
JosefKrieglstein <JosefKrieglstein@users.noreply.github.com>
2017-02-18 21:02:52 +0100

    Create  HDO-VT.py

---
+++ HDO-VT.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import urllib2
+import httplib
+
+# Domain you want to post to: localhost would be an emoncms installation on your own laptop
+# this could be changed to emoncms.org to post to emoncms.org
+server = "emoncms.trenet.org"
+
+# Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
+# To post to emoncms.org change this to blank: ""
+emoncmspath = ""
+
+# Write apikey of emoncms account
+apikey = "53863683c9745504ab727de6f736c94b"
+
+# Node id youd like the emontx to appear as
+nodeid = 14
+
+# Send to emoncms
+# initialization HTTPConnection
+conn = httplib.HTTPConnection(server)
+conn.request("GET", "/"+emoncmspath+"/input/post.json?&node="+str(nodeid)+"&json="+"NT:1,VT:0"+"&apikey="+apikey)
+conn.close()
